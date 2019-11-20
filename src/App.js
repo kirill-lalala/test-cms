a8602bf2f7e677756f59f5ccef7706d6c810bdaf
kirill_lalala <mr.ilikov@gmail.com>
2019-11-21 01:05:22 +0300

    rubber layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Sidebar from "./Components/Sidebar/Sidebar";
+import styles from  './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+
+        <div className={styles.sidebar}>
+          <Sidebar/>
+        </div>
+
+        <div className={styles.content}>
+          content
+        </div>
+      </div>
+
+      <div className={styles.footer}>
+        <div>lorem</div>
+        <div>Lorem ipsum.</div>
+        <div>Lorem ipsum dolor sit.</div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Sidebar.module.scss';
+
+const Sidebar = props => {
+    return (
+        <div className={styles.div}>
+            <div className={styles.logo}>
+                <img src="" alt="logo"/>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
